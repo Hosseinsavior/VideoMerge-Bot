2474e663fabe110328d182de45b6156f90910690
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2024-10-31 01:43:17 +0330

    Update database.py

--- helpers/database/database.py
+++ helpers/database/database.py
@@ -1,69 +1,80 @@
-# (c) @AbirHasan2005
+import asyncio
+import time
+from configs import Config
+from helpers.database.access_db import db
+from helpers.display_progress import progress_for_pyrogram, humanbytes
+from humanfriendly import format_timespan
+from pyrogram import Client
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
-import datetime
-import motor.motor_asyncio
+async def create_reply_markup():
+    """Create the reply markup for the uploaded video."""
+    return InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
+            [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
+             InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+        ]
+    )
 
+async def upload_video(bot: Client, cb: CallbackQuery, merged_vid_path: str, width: int, height: int, duration: int, video_thumbnail: str, file_size: int):
+    """
+    Upload a video or document to Telegram based on user settings.
 
-class Database:
+    Args:
+        bot (Client): The Pyrogram Client instance.
+        cb (CallbackQuery): The callback query object.
+        merged_vid_path (str): The path to the merged video.
+        width (int): Width of the video.
+        height (int): Height of the video.
+        duration (int): Duration of the video in seconds.
+        video_thumbnail (str): Path to the video thumbnail.
+        file_size (int): Size of the file in bytes.
 
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
+    Returns:
+        None
+    """
+    try:
+        sent_ = None
+        is_upload_as_doc = await db.get_upload_as_doc(cb.from_user.id)
+        c_time = time.time()
+        
+        caption = Config.CAPTION.format((await bot.get_me()).username) + f"\n\n**File Name:** `{merged_vid_path.rsplit('/', 1)[-1]}`\n**Duration:** `{format_timespan(duration)}`\n**File Size:** `{humanbytes(file_size)}`"
 
-    def new_user(self, id):
-        return dict(
-            id=id,
-            join_date=datetime.date.today().isoformat(),
-            upload_as_doc=False,
-            thumbnail=None,
-            generate_ss=False,
-            generate_sample_video=False
+        if not is_upload_as_doc:
+            sent_ = await bot.send_video(
+                chat_id=cb.message.chat.id,
+                video=merged_vid_path,
+                width=width,
+                height=height,
+                duration=duration,
+                thumb=video_thumbnail,
+                caption=caption,
+                progress=progress_for_pyrogram,
+                progress_args=("Uploading Video ...", cb.message, c_time),
+                reply_markup=await create_reply_markup()
+            )
+        else:
+            sent_ = await bot.send_document(
+                chat_id=cb.message.chat.id,
+                document=merged_vid_path,
+                caption=caption,
+                thumb=video_thumbnail,
+                progress=progress_for_pyrogram,
+                progress_args=("Uploading Video ...", cb.message, c_time),
+                reply_markup=await create_reply_markup()
+            )
+
+        await asyncio.sleep(Config.TIME_GAP)
+        forward_ = await sent_.forward(chat_id=Config.LOG_CHANNEL)
+        await forward_.reply_text(
+            text=f"**User:** [{cb.from_user.first_name}](tg://user?id={str(cb.from_user.id)})\n**Username:** `{cb.from_user.username}`\n**UserID:** `{cb.from_user.id}`",
+            disable_web_page_preview=True,
+            quote=True
         )
-
-    async def add_user(self, id):
-        user = self.new_user(id)
-        await self.col.insert_one(user)
-
-    async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return True if user else False
-
-    async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
-
-    async def get_all_users(self):
-        all_users = self.col.find({})
-        return all_users
-
-    async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
-
-    async def set_upload_as_doc(self, id, upload_as_doc):
-        await self.col.update_one({'id': id}, {'$set': {'upload_as_doc': upload_as_doc}})
-
-    async def get_upload_as_doc(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('upload_as_doc', False)
-
-    async def set_thumbnail(self, id, thumbnail):
-        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
-
-    async def get_thumbnail(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('thumbnail', None)
-
-    async def set_generate_ss(self, id, generate_ss):
-        await self.col.update_one({'id': id}, {'$set': {'generate_ss': generate_ss}})
-
-    async def get_generate_ss(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('generate_ss', False)
-
-    async def set_generate_sample_video(self, id, generate_sample_video):
-        await self.col.update_one({'id': id}, {'$set': {'generate_sample_video': generate_sample_video}})
-
-    async def get_generate_sample_video(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('generate_sample_video', False)
+    except Exception as err:
+        print(f"Failed to Upload Video!\nError: {err}")
+        try:
+            await cb.message.edit(f"Failed to Upload Video!\n**Error:**\n`{err}`")
+        except Exception as edit_err:
+            print(f"Failed to edit message: {edit_err}")
